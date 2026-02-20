41854ed51ef67c785cce1cb916bb9f77e2dac2d6
SaraSgattoni <sara.sgattoni@studenti.unicam.it>
2026-02-20 14:23:10 +0100

    added sanction and support request reply dtos

---
+++ src/main/java/model/dto/ApplySanctionDTO.java
@@ -0,0 +1,20 @@
+package model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import model.enums.ReportResolution;
+
+@Getter
+@NoArgsConstructor
+public class ApplySanctionDTO {
+
+    private ReportResolution sanctionType;
+    private String reason;
+    private int points;
+
+    public ApplySanctionDTO(ReportResolution sanctionType, String reason, int points) {
+        this.sanctionType = sanctionType;
+        this.reason = reason;
+        this.points = points;
+    }
+}
